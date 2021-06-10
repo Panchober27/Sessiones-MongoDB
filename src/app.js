3d0f4c570a75c18e41e37bf37f861b0207f348de
PanchoPC2 <58098908+Panchober27@users.noreply.github.com>
2021-06-09 21:27:12 -0400

    Upload to GitHub🐱‍👤

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const mongodbsession = require("connect-mongodb-session")(session);
+const app = express();
+const port = 3000;
+const mongoURI = "mongodb://localhost:27017/sessions";
+
+// Conexion a MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/sessions", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log("Mongo DB conected!");
+  });
+
+const store = new mongodbsession({
+  uri: mongoURI,
+  collection: "mySessions",
+});
+
+// Seteo de las vistas.
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "key that will sign the cooke",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.get("/", (req, res) => {
+  req.session.isAuth = true;
+  // console.log(req.session);
+  console.log(req.session.id);
+  res.send("sesiones y cookies :)");
+});
+
+app.listen(port, () => {
+  console.log(`On port: ${port}`);
+});
